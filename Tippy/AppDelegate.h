89ad2a9f28d1351e275dab17b67ed80519d8d0a7
Megan Yu <meganyu@fb.com>
2020-06-23 13:02:41 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by meganyu on 6/23/20.
+//  Copyright © 2020 meganyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tippy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tippy
+//
+//  Created by meganyu on 6/23/20.
+//  Copyright © 2020 meganyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
